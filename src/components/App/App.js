a49712fc93994044cb203a5afac04402faee71b7
OxanaDoroshenko <donotsettlebebrave@gmail.com>
2016-12-24 12:54:56 +0300

    first changes in main page layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -55,7 +55,7 @@ class App extends Component {
 
   render() {
     return !this.props.error ? (
-      <div>
+      <div className="app">
         <Header />
         {this.props.children}
         <Feedback />
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ class Footer extends Component {
 
   render() {
     return (
-      <div className={s.root}>
+      <footer className={s.root}>
         <div className={s.container}>
           <span className={s.text}>© Your Company</span>
           <span className={s.spacer}>·</span>
@@ -26,7 +26,7 @@ class Footer extends Component {
           <span className={s.spacer}>·</span>
           <Link className={s.link} to="/not-found">Not Found</Link>
         </div>
-      </div>
+      </footer>
     );
   }
 
